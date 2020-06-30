ce915b57ec12a547656e62e4072e63e9ba1364ea
joao <j@vmelo.net>
2020-06-29 21:24:28 -0300

    firebase emulators integrated to dev worflow

--- client/src/core/firebase/firebase.js
+++ client/src/core/firebase/firebase.js
@@ -1,5 +1,15 @@
+import { appEnviroment } from '__cli/core/meta';
+
 const fireapp = firebase.app();
 const fireauth = fireapp.auth();
 const firedb = fireapp.firestore();
+
+// connect to firestore locally
+if (appEnviroment() === 'dev') {
+  firedb.settings({
+    host: 'localhost:8080',
+    ssl: false
+  });
+}
 
 export { fireapp, fireauth, firedb };
