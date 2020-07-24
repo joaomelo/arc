83e66652afcfcb76d6999ca714ac34f52da8b897
Joao Melo <j@melo.plus>
2020-07-24 20:40:08 -0300

    removed props to assure i18n reactivity

--- client/src/core/components/actions/save-cancel.vue
+++ client/src/core/components/actions/save-cancel.vue
@@ -3,34 +3,22 @@
     <BaseButton
       color="secondary"
       icon="mdi-backspace"
-      :text="cancelLabel"
+      :text="$t('actions.cancel')"
       @click="$emit('cancel')"
     />
     <BaseButton
       color="success"
       icon="mdi-content-save"
-      :text="saveLabel"
+      :text="$t('actions.save')"
       @click="$emit('save')"
     />
   </div>
 </template>
 
 <script>
-import { i18n } from '__cli/core/i18n';
-
 import BaseButton from './base-button';
 export default {
   name: 'SaveCancel',
-  components: { BaseButton },
-  props: {
-    cancelLabel: {
-      type: String,
-      default: i18n.t('actions.cancel')
-    },
-    saveLabel: {
-      type: String,
-      default: i18n.t('actions.save')
-    }
-  }
+  components: { BaseButton }
 };
 </script>
