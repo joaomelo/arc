d3dcaf3df429e043162b46f656b4e857d74a9693
Joao Melo <j@melo.plus>
2020-07-25 11:39:30 -0300

    moved binding to collection file

--- client/src/modules/profiles/domain/collection.js
+++ client/src/modules/profiles/domain/collection.js
@@ -1,10 +1,14 @@
-import { Collection } from '__cli/core/collection';
+import { Collection, bindCollectionToAuth } from '__cli/core/collection';
 import { authState } from '__cli/core/auth';
 
 const profilesCollection = new Collection('profiles');
+
+function plugCollection () {
+  bindCollectionToAuth(profilesCollection);
+}
 
 function getCurrentProfile () {
   return profilesCollection.getItem(authState.userData.uid);
 }
 
-export { profilesCollection, getCurrentProfile };
+export { profilesCollection, getCurrentProfile, plugCollection };
