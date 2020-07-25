a60ece830f671fe851524f26d151d9b43a58a5cb
Joao Melo <j@melo.plus>
2020-07-25 11:50:23 -0300

    added options argument

--- client/src/core/collection/auth-bind.js
+++ client/src/core/collection/auth-bind.js
@@ -1,10 +1,10 @@
 import { AUTH_STATUSES, authStateChanged } from '__cli/core/auth';
 
-function bindCollectionToAuth (collection) {
+function bindCollectionToAuth (collection, options) {
   return authStateChanged.subscribe(({ status, oldStatus }) => {
     if (status === oldStatus) return;
     const action = status === AUTH_STATUSES.SIGNEDIN ? 'connect' : 'disconnect';
-    collection[action]();
+    collection[action](options);
   });
 }
 
