5d98622170598616b98ddc40751b9c526691792c
Joao Melo <j@melo.plus>
2020-10-10 14:29:00 -0300

    bugfix: handleError was called twice

--- server/src/core/error/middleware.js
+++ server/src/core/error/middleware.js
@@ -1,7 +1,6 @@
 import { handleError } from './handle';
 
 function errorMiddleware (err, req, res, next) {
-  handleError(err);
   const isOperationalError = handleError(err);
   if (isOperationalError) {
     res.status(err.httpStatus).send(err.message);
