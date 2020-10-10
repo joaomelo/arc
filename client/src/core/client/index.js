bc99649352d92359256330912dadaa86ade9a2bd
Joao Melo <j@melo.plus>
2020-10-10 14:28:23 -0300

    raise excepction only if 500

--- client/src/core/client/index.js
+++ client/src/core/client/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const client = axios.create({
   baseURL: 'http://localhost:3000',
-  validateStatus: () => true
+  validateStatus: status => status < 500
 });
 
 export { client };
