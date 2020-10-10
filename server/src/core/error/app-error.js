9af4af696750d19fdce2676e25f6e40d683b7bac
Joao Melo <j@melo.plus>
2020-10-10 14:28:44 -0300

    message is passed to super class

--- server/src/core/error/app-error.js
+++ server/src/core/error/app-error.js
@@ -2,7 +2,6 @@ class AppError extends Error {
   constructor ({ code, message, httpStatus, isOperational }) {
     super(message);
     this.code = code;
-    this.message = message;
     this.isOperational = isOperational;
     this.httpStatus = httpStatus;
     Error.captureStackTrace(this, AppError);
