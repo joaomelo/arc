91ac82e3859ccab471aa3050b89c4c8196a2e58f
Joao Melo <j@melo.plus>
2020-11-05 19:33:40 -0300

    strict html header for history mode

--- server/src/core/router/app.js
+++ server/src/core/router/app.js
@@ -11,7 +11,15 @@ export function createApp () {
   const app = express();
 
   app.use(express.json());
-  app.use(history({ verbose: !isProduction() }));
+
+  // used to enable history mode in react router
+  // it rewrites all html request to the index.html
+  app.use(history({
+    verbose: !isProduction(),
+    // option bellows enforces that the middleware will only act
+    // if accept header 'text/html' is present
+    htmlAcceptHeaders: ['text/html']
+  }));
 
   const staticRoot = resolve(__dirname, 'public');
   app.use(express.static(staticRoot));
