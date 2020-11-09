e843120e0cee08c1f643645404cb7b82f1694aa8
Joao Melo <j@melo.plus>
2020-11-09 20:52:31 -0300

    enabled cors

--- server/src/core/router/app.js
+++ server/src/core/router/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { errorMiddleware } from '__ser/core/error';
 import { feature as userFeature } from '__ser/modules/users';
 import { authenticateToken } from './auth-token';
@@ -6,6 +7,15 @@ import { setFeatures } from './features';
 
 export function createApp () {
   const app = express();
+
+  app.use(cors({
+    origin: process.env.ORIGIN,
+    // enables pre-fligth cache (Access-Control-Max-Age) for 24h
+    // since the origin server shouldn't change regulary.
+    // this is the maximium supported value by firefox, others
+    // browsers are more restrict and will cap the value.
+    maxAge: 86400
+  }));
 
   app.use(express.json());
   app.use(authenticateToken);
