6c42d7954f5f1b21a004f4d0e57bf2cf80abff90
joão melo <j@melo.plus>
2021-03-29 16:44:20 -0300

    feat: basic sign-in ui

--- src/features/auth/web/sign-in-view.jsx
+++ src/features/auth/web/sign-in-view.jsx
@@ -4,16 +4,46 @@ export function SignInView ({ onSubmit }) {
   const { register, handleSubmit } = useForm();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Email:</label>
-        <input name="email" ref={register({ required: true })} />
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in
+        </h2>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="inputEmail" className="sr-only">Email address</label>
+              <input
+                id="inputEmail"
+                name="email"
+                ref={register({ required: true })}
+                type="email"
+                autoComplete="email"
+                required
+                placeholder="Email address"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label htmlFor="inputPassword" className="sr-only">Password</label>
+              <input
+                id="inputPassword"
+                name="password"
+                ref={register({ required: true })}
+                type="password"
+                autoComplete="current-password"
+                required
+                placeholder="Password"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Sign in
+          </button>
+        </form>
       </div>
-      <div>
-        <label>Password:</label>
-        <input name="password" ref={register({ required: true })} />
-      </div>
-      <input type="submit" />
-    </form>
+    </div>
   );
 }
