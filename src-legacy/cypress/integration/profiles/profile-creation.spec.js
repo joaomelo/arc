c45ab229fac258db66c10b2791e2bc9587595a68
joão melo <j@melo.plus>
2021-04-12 21:31:10 -0300

    chore: removed unused legacy code

--- src-legacy/cypress/integration/profiles/profile-creation.spec.js
+++
@@ -1,9 +0,0 @@
-describe('profile creation', () => {
-  it.skip('create a user profile with default preferences', () => {
-    cy.get('div').then(() => expect(false).to.be.true);
-  });
-
-  it.skip('only allow profile creation when signing up new user', () => {
-    cy.get('div').then(() => expect(false).to.be.true);
-  });
-});
--- src-legacy/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-// https://on.cypress.io/custom-commands
-
-Cypress.Commands.add('signUpManual', (email, password) => {
-  cy.visit('/sign-up');
-  cy.get('#control-email').type(email);
-  cy.get('#control-password').type(password);
-  cy.get('#control-repeat-password').type(password);
-  cy.get('#action-sign-up').click();
-});
